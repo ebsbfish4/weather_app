5ab7c51a036c037d703d6de2b65dedd3706339b3
Drew Lanigan <drew.lanigan@gmail.com>
2018-04-28 11:42:23 -0500

    initial

---
+++ src/containers/search_bar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+    // Don't submit the form using typical POST method
+    event.preventDefault();
+
+    // Fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
+
+// This causes the action creator, whenever it is called and returns an action
+// makes sure the action flows down through the middleware and into reducers
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather}, dispatch);
+}
+
+// Passing null in here, all we are saying is I know redux is maintaining state
+// but we do not care about it
+export default connect(null, mapDispatchToProps)(SearchBar);
